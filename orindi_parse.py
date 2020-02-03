7f90e5057038c6787b8d84f3926c5183bb3a6520
Steven Saus <steven@stevesaus.com>
2020-02-03 05:24:36 -0500

    Decided on format for filename

--- orindi_parse.py
+++ orindi_parse.py
@@ -7,6 +7,7 @@
 import sys, getopt, re 
 import mailparser
 import datetime,time
+from time import strftime,localtime
 argv = None
 import configparser
 import os
@@ -20,6 +21,7 @@ from pathlib import Path
 
 appname = "orindi"
 appauthor = "Steven Saus"
+configdir = user_config_dir(appname)
 if not os.path.isdir(configdir):
     os.makedirs(user_config_dir(appname))
 ini = os.path.join(configdir,'orindi.ini')
@@ -28,56 +30,42 @@ ini = os.path.join(configdir,'orindi.ini')
 ########################################################################
 # Parsing that email!
 ########################################################################
-def parse_that_email():
-    #print 'Number of arguments:', len(sys.argv), 'arguments.'
-    #print 'Argument List:', str(sys.argv)
-    #messagefile = open(tmp,'r')
-    messagefile = ''
-    messagefile = (sys.argv[1])  # using ini here, oh procmail copy to handle
+def parse_that_email(messagefile):
     print ('Message file is ' + messagefile)
 
-
-    # If the e-mail headers are in a file, uncomment these two lines:
     with open(messagefile,'r') as fp:
          mail = mailparser.parse_from_file_obj(fp)
 
-    #print (mail.body)
-
-    #  Now the header items can be accessed as a dictionary:
     print('To: ' + str(mail.to))
 
     date_time_obj = datetime.datetime.strptime(str(mail.date),"%Y-%m-%d %H:%M:%S")
-    #thetime=time.strftime("%Y%m%d%H%M%S",mail.date)
-    # use this plus something else as filename - oh, NOW. Use now and then 
-    # time sleep to make sure it's all different. 
-    #time.sleep(2)
     print (str(date_time_obj.date()) + '-' + str(date_time_obj.time()))
 
     ##########################################################################
     # Match the fromstring to the outdirectory
     ##########################################################################
     for x in sections:
-    if "feed" in (str.lower(x)):
-        
-        keyword=config[x]['keyword']
-        FromList = str.lower(config[x]['from'])
-        SubjectList = str.lower(config[x]['subject'])
-        if str(mail.from_) in FromList.split():
-            outdir = keyword
-        else:
-            if str(mail.subject) in SubjectList.split():
+        if "feed" in (str.lower(x)):
+            keyword=config[x]['keyword']
+            FromList = str.lower(config[x]['from'])
+            SubjectList = str.lower(config[x]['subject'])
+            if str(mail.from_) in FromList.split():
                 outdir = keyword
+            else:
+                if str(mail.subject) in SubjectList.split():
+                    outdir = keyword
+#TODO:  The subject is a string fragment match, not a word match, gah
+#TODO:  The directory is attached to the base outdir.
 
-    # You can also access the parts of the addresses:
-    #define postfile
+# Using now/localtime + time sleep to make sure it's all different for filename of outfile. 
+
+    time.sleep(2)
+    date_published = localtime()  
+    thetime=time.strftime("%Y%m%d%H%M%S",localtime())
+    print ('Date2: ' + thetime)
+    #define postfile name
     #  f = open(postfile, 'a')
-    #does not work
-    #thetime=time.strftime("%Y%m%d%H%M%S",mail.date)
-    #print ('Date2: ' + thetime)
-    #print('Body: ' + mail.body)
-    #print('text: ' + str(mail.text_plain))
-    #print('html: ' + str(mail.text_html))
-    #messagefile.close
+
     print ('---')
     print('Title: ' + str(mail.subject))
     print('Description: ' + str(mail.subject))
@@ -87,10 +75,11 @@ def parse_that_email():
     print('Template: index')
     print ('---')
     #if mail.text_html:
-        #print('html: ' + str(mail.text_html))
+    #    print('html: ' + str(mail.text_html))
     #else:
-        #print('text: ' + str(mail.text_plain))
+    #    print('text: ' + str(mail.text_plain))
     #f.close
+    fp.close
 
 ########################################################################
 # Read ini section
@@ -103,3 +92,6 @@ sections=config.sections()
 ########################################################################
 # Main function
 ########################################################################
+infile = ''
+infile = (sys.argv[1])  # using ini here, oh procmail copy to handle
+parse_that_email(infile)
